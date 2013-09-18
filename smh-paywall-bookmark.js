4157cfc0bc510ed63ea8f5506ec89757233b3295
Andrew Peel <andrew@apparent.com.au>
2013-09-18 10:43:49 +1000

    Updated to work with The Age

--- smh-paywall-bookmark.js
+++ smh-paywall-bookmark.js
@@ -1,1 +1,4 @@
-javascript: (function() { document.getElementById("subscription-overlay").style.display = "none"; document.getElementsByTagName("body")[0].removeAttribute("style");})()
+javascript: (function() { document.getElementById("subscription-overlay").style.display = "none"; document.getElementsByTagName("body")[0].removeAttribute("style"); document.querySelectorAll(".article-counter")[0].style.display = "none"; })()
+
+
+ 
